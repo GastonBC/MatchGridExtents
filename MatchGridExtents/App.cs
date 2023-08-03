45ec12542ffc8c85a699b3c082e4d15cc139f330
Gaston <gastonbalparda@gmail.com>
2023-08-03 15:25:19 -0300

    Add project files.

---
+++ MatchGridExtents/App.cs
@@ -0,0 +1,55 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Utilities;
+
+namespace MatchGridExtents
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.DB.Macros.AddInId("4524a1b8-b55a-48d8-974f-38fa3a377290")]
+    public class ThisApplication : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication uiApp)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication uiApp)
+        {
+
+            try
+            {
+                #region GAS ADDIN BOILERPLATE
+                // Assembly that contains the invoke method
+                string exeConfigPath = Utils.GetExeConfigPath("MatchGridExtents.dll");
+
+                // Finds and creates the tab, finds and creates the panel
+                RibbonPanel DefaultPanel = Utils.GetRevitPanel(uiApp, GlobalVars.PANEL_NAME);
+                #endregion
+
+                // Button configuration
+                string MatchGridExtentsName = "Default\nName";
+                PushButtonData MatchGridExtentsData = new PushButtonData(MatchGridExtentsName, MatchGridExtentsName, exeConfigPath, "MatchGridExtents.ThisCommand");
+                MatchGridExtentsData.LargeImage = Utils.RetriveImage("MatchGridExtents.Resources.MatchGridExtents32x32.ico", Assembly.GetExecutingAssembly()); // Pushbutton image
+                MatchGridExtentsData.ToolTip = "";
+                DefaultPanel.AddItem(MatchGridExtentsData); // Add pushbutton
+
+                return Result.Succeeded;
+            }
+
+
+            catch (Exception ex)
+            {
+                Utils.CatchDialog(ex);
+                return Result.Failed;
+            }
+        }
+    }
+}
---
+++ MatchGridExtents/Cmd.cs
@@ -0,0 +1,34 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace MatchGridExtents
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.DB.Macros.AddInId("4524a1b8-b55a-48d8-974f-38fa3a377290")]
+    public class ThisCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                UIDocument uidoc = commandData.Application.ActiveUIDocument;
+                Document doc = uidoc.Document;
+
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                Utils.CatchDialog(ex);
+                return Result.Failed;
+            }
+        }
+    }
+}
